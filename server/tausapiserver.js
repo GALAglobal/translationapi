42bb1dea60f43152913174215567657ef4ed6e73
Dr. Klemens Waldhör <info@heartsome.de>
2014-09-24 15:58:24 +0200

    v2.0b
    see changes in file

--- server/tausapiserver.js
+++ server/tausapiserver.js
@@ -25,12 +25,31 @@ SOFTWARE.
 
 /*
 Author: Klemens Waldhör
-Version 2.0a
+Version 2.0b / 24.09.2014
 
 TAUS Translation API – Version 2.0
 TAUS Technical Specification -­ A Common Translation Services API - August 2014
 2.0 from 20.08.2014
 */
+
+/* Note: Current version requires express 3.0; will be changed to 4.0 with the next version! */
+
+/*
+* Pattern: <url>/v2.0/<request method>
+* Example: PUT: http://localhost:3412/v2.0/translation/confirm/697a6639-f8e4-47b1-bc77-865b6a9a53c2
+*/
+
+/*
+ * Changes:
+ * v2.0a
+ * - added patch methods
+ * - added license, header
+ * v2.0b:
+ * - Path now starts with 2.0
+ * - moved various functions outside the direct call app.put('/v2.0/translation/', function(req, res)) to app.put('/v2.0/translation/confirm', confirm) with confirm a function object 
+ * - added comments
+*/
+
 
 var express = require('express');
 var js2xmlparser = require("js2xmlparser");
@@ -174,7 +193,13 @@ function findTranslationRequestIndex(id)
 
 app.use(express.bodyParser());
 
-app.get('/translation/', function(req, res) 
+/*********************************************************************************************************/
+/* 
+ * Get all  requests 
+ * requires no id, will support filter criteria too
+ */
+
+app.get('/v2.0/translation/', function(req, res) 
 {
 	res.set('Content-Type', 'application/json');
 	res.statusCode = 200;
@@ -186,7 +211,13 @@ app.get('/translation/', function(req, res)
   console.log(res);
 });
 
-app.get('/translation/:id', function(req, res) 
+/*********************************************************************************************************/
+/* 
+ * Get a  request 
+ * requires an id
+ */
+
+app.get('/v2.0/translation/:id', function(req, res) 
 {
 	var q = findTranslationRequest(req.params.id);
 	if (q != null)
@@ -201,7 +232,13 @@ app.get('/translation/:id', function(req, res)
 	return res.send('Error 404: No translationRequest ' + req.params.id + ' found');
 });
 
-app.get('/translation/status/:id', function(req, res) 
+/*********************************************************************************************************/
+/* 
+ * Get status of a request 
+ * requires an id
+ */
+ 
+app.get('/v2.0/translation/status/:id', function(req, res) 
 {
 	// console.log(req);
 	var q = findTranslationRequest(req.params.id);
@@ -221,69 +258,20 @@ app.get('/translation/status/:id', function(req, res)
 	return res.send('Error 404: No translationRequest ' + req.params.id + ' found');
 });
 
-app.put('/translation', function(req, res) 
+/*********************************************************************************************************/
+/* 
+ * Update a request 
+ * requires an id - both put and patch
+ */
+
+function update(req, res) 
 {
 	var request = req.body.translationRequest;
 	if (request == undefined)
 	{
 		console.log(req);
 		res.statusCode = 400;
-		return res.send('Error 400: Post syntax incorrect. translationRequest for PUT property missing');
-	}
-	
-	var i = findTranslationRequestIndex(req.params.id); // need to adapt to the real id!
-	if (i != null)
-	{
-		if (request.mt != undefined)
-			translationRequests[i].translationRequest.mt = 					request.mt;
-		if (request.sourceLanguage != undefined)
-			translationRequests[i].translationRequest.sourceLanguage =  	request.sourceLanguage;
-		if (request.targetLanguage != undefined)
-			translationRequests[i].translationRequest.targetLanguage =  	request.targetLanguage;
-		if (request.source != undefined)
-			translationRequests[i].translationRequest.source = 				request.source;
-		if (request.target != undefined)
-			translationRequests[i].translationRequest.target = 				request.target;
-		if (request.crowd != undefined)
-			translationRequests[i].translationRequest.crowd =				request.crowd;	
-		if (request.professional != undefined)
-			translationRequests[i].translationRequest.professional =		request.professional;	
-		if (request.postedit != undefined)
-			translationRequests[i].translationRequest.postedit =			request.postedit;	
-		if (request.comment != undefined)
-			translationRequests[i].translationRequest.comment =				request.comment;
-		if (request.translator != undefined)
-			translationRequests[i].translationRequest.translator =			request.translator;	
-		if (request.owner != undefined)
-			translationRequests[i].translationRequest.owner =				request.owner;
-		if (request.status != undefined)
-			translationRequests[i].translationRequest.status =				request.status;
-		translationRequests[i].translationRequest.modificationDate =		new Date();
-		
-		if (translationRequests[i].translationRequest.updateCounter == undefined)
-			translationRequests[i].translationRequest.updateCounter = 1;
-		else if (translationRequests[i].translationRequest.updateCounter == null)
-			translationRequests[i].translationRequest.updateCounter = 1;
-		else
-			translationRequests[i].translationRequest.updateCounter =		request.updateCounter + 1;
-		saveTranslationRequests();
-		res.statusCode = 201;
-		res.json(translationRequests[i]);
-		return;
-	}
-
-	res.statusCode = 404;
-	return res.send('Error 404: No translationRequest ' + req.params.id + ' found');
-});
-
-app.put('/translation/:id', function(req, res) 
-{
-	var request = req.body.translationRequest;
-	if (request == undefined)
-	{
-		console.log(req);
-		res.statusCode = 400;
-		return res.send('Error 400: Post syntax incorrect. translationRequest for PUT property missing');
+		return res.send('Error 400: Put/Patch syntax incorrect. translationRequest for PUT property missing');
 	}
 	
 	var i = findTranslationRequestIndex(req.params.id); // need to adapt to the real id!
@@ -329,64 +317,18 @@ app.put('/translation/:id', function(req, res)
 
 	res.statusCode = 404;
 	return res.send('Error 404: No translationRequest ' + req.params.id + ' found');
-});
+}
 
-app.patch('/translation/:id', function(req, res) 
-{
-	var request = req.body.translationRequest;
-	if (request == undefined)
-	{
-		console.log(req);
-		res.statusCode = 400;
-		return res.send('Error 400: Post syntax incorrect. translationRequest for PUT property missing');
-	}
-	
-	var i = findTranslationRequestIndex(req.params.id); // need to adapt to the real id!
-	if (i != null)
-	{
-		if (request.mt != undefined)
-			translationRequests[i].translationRequest.mt = 					request.mt;
-		if (request.sourceLanguage != undefined)
-			translationRequests[i].translationRequest.sourceLanguage =  	request.sourceLanguage;
-		if (request.targetLanguage != undefined)
-			translationRequests[i].translationRequest.targetLanguage =  	request.targetLanguage;
-		if (request.source != undefined)
-			translationRequests[i].translationRequest.source = 				request.source;
-		if (request.target != undefined)
-			translationRequests[i].translationRequest.target = 				request.target;
-		if (request.crowd != undefined)
-			translationRequests[i].translationRequest.crowd =				request.crowd;	
-		if (request.professional != undefined)
-			translationRequests[i].translationRequest.professional =		request.professional;	
-		if (request.postedit != undefined)
-			translationRequests[i].translationRequest.postedit =			request.postedit;	
-		if (request.comment != undefined)
-			translationRequests[i].translationRequest.comment =				request.comment;
-		if (request.translator != undefined)
-			translationRequests[i].translationRequest.translator =			request.translator;	
-		if (request.owner != undefined)
-			translationRequests[i].translationRequest.owner =				request.owner;
-		if (request.status != undefined)
-			translationRequests[i].translationRequest.status =				request.status;
-		translationRequests[i].translationRequest.modificationDate =		new Date();
-		
-		if (translationRequests[i].translationRequest.updateCounter == undefined)
-			translationRequests[i].translationRequest.updateCounter = 1;
-		else if (translationRequests[i].translationRequest.updateCounter == null)
-			translationRequests[i].translationRequest.updateCounter = 1;
-		else
-			translationRequests[i].translationRequest.updateCounter =		translationRequests[i].translationRequest.updateCounter + 1;
-		saveTranslationRequests();
-		res.statusCode = 200;
-		res.json(translationRequests[i]);
-		return;
-	}
+app.put('/v2.0/translation/:id', update);
+app.patch('/v2.0/translation/:id', update);
 
-	res.statusCode = 404;
-	return res.send('Error 404: No translationRequest ' + req.params.id + ' found');
-});
+/*********************************************************************************************************/
+/* 
+ * Confirm a request 
+ * requires an id - both put and patch
+ */
 
-app.put('/translation/confirm/:id', function(req, res) 
+function confirm(req, res) 
 {
 	var i = findTranslationRequestIndex(req.params.id); // need to adapt to the real id!
 	if (i != null)
@@ -408,33 +350,18 @@ app.put('/translation/confirm/:id', function(req, res)
 
 	res.statusCode = 404;
 	return res.send('Error 404: No translationRequest ' + req.params.id + ' found');
-});
+}
 
-app.patch('/translation/confirm/:id', function(req, res) 
-{
-	var i = findTranslationRequestIndex(req.params.id); // need to adapt to the real id!
-	if (i != null)
-	{
-		translationRequests[i].translationRequest.status =					"confirmed";
-		translationRequests[i].translationRequest.modificationDate =		new Date();
-		
-		if (translationRequests[i].translationRequest.updateCounter == undefined)
-			translationRequests[i].translationRequest.updateCounter = 1;
-		else if (translationRequests[i].translationRequest.updateCounter == null)
-			translationRequests[i].translationRequest.updateCounter = 1;
-		else
-			translationRequests[i].translationRequest.updateCounter =		translationRequests[i].translationRequest.updateCounter + 1;
-		saveTranslationRequests();
-		res.statusCode = 200;
-		res.json(translationRequests[i]);
-		return;
-	}
+app.put('/v2.0/translation/confirm/:id', confirm);
+app.patch('/v2.0/translation/confirm/:id', confirm);
 
-	res.statusCode = 404;
-	return res.send('Error 404: No translationRequest ' + req.params.id + ' found');
-});
-
-app.put('/translation/cancel/:id', function(req, res) 
+/*********************************************************************************************************/
+/* 
+ * Cancel a request 
+ * requires an id - both put and patch
+ */
+ 
+function cancel(req, res) 
 {
 	var i = findTranslationRequestIndex(req.params.id); // need to adapt to the real id!
 	if (i != null)
@@ -456,33 +383,18 @@ app.put('/translation/cancel/:id', function(req, res)
 
 	res.statusCode = 404;
 	return res.send('Error 404: No translationRequest ' + req.params.id + ' found');
-});
+}
 
-app.patch('/translation/cancel/:id', function(req, res) 
-{
-	var i = findTranslationRequestIndex(req.params.id); // need to adapt to the real id!
-	if (i != null)
-	{
-		translationRequests[i].translationRequest.status =					"cancel";
-		translationRequests[i].translationRequest.modificationDate =		new Date();
-		
-		if (translationRequests[i].translationRequest.updateCounter == undefined)
-			translationRequests[i].translationRequest.updateCounter = 1;
-		else if (translationRequests[i].translationRequest.updateCounter == null)
-			translationRequests[i].translationRequest.updateCounter = 1;
-		else
-			translationRequests[i].translationRequest.updateCounter =		translationRequests[i].translationRequest.updateCounter + 1;
-		saveTranslationRequests();
-		res.statusCode = 200;
-		res.json(translationRequests[i]);
-		return;
-	}
+app.put('/v2.0/translation/cancel/:id', cancel);
+app.patch('/v2.0/translation/cancel/:id', cancel);
 
-	res.statusCode = 404;
-	return res.send('Error 404: No translationRequest ' + req.params.id + ' found');
-});
+/*********************************************************************************************************/
+/* 
+ * Reject a request 
+ * requires an id - both put and patch
+ */
 
-app.put('/translation/reject/:id', function(req, res) 
+function reject(req, res) 
 {
 	var i = findTranslationRequestIndex(req.params.id); // need to adapt to the real id!
 	if (i != null)
@@ -504,33 +416,18 @@ app.put('/translation/reject/:id', function(req, res)
 
 	res.statusCode = 404;
 	return res.send('Error 404: No translationRequest ' + req.params.id + ' found');
-});
+}
 
-app.patch('/translation/reject/:id', function(req, res) 
-{
-	var i = findTranslationRequestIndex(req.params.id); // need to adapt to the real id!
-	if (i != null)
-	{
-		translationRequests[i].translationRequest.status =					"reject";
-		translationRequests[i].translationRequest.modificationDate =		new Date();
-		
-		if (translationRequests[i].translationRequest.updateCounter == undefined)
-			translationRequests[i].translationRequest.updateCounter = 1;
-		else if (translationRequests[i].translationRequest.updateCounter == null)
-			translationRequests[i].translationRequest.updateCounter = 1;
-		else
-			translationRequests[i].translationRequest.updateCounter =		translationRequests[i].translationRequest.updateCounter + 1;
-		saveTranslationRequests();
-		res.statusCode = 200;
-		res.json(translationRequests[i]);
-		return;
-	}
+app.put('/v2.0/translation/reject/:id', reject);
+app.patch('/v2.0/translation/reject/:id', reject);
 
-	res.statusCode = 404;
-	return res.send('Error 404: No translationRequest ' + req.params.id + ' found');
-});
+/*********************************************************************************************************/
+/* 
+ * Accept a request 
+ * requires an id - both put and patch
+ */
 
-app.put('/translation/accept/:id', function(req, res) 
+function accept (req, res) 
 {
 	var i = findTranslationRequestIndex(req.params.id); // need to adapt to the real id!
 	if (i != null)
@@ -552,51 +449,26 @@ app.put('/translation/accept/:id', function(req, res)
 
 	res.statusCode = 404;
 	return res.send('Error 404: No translationRequest ' + req.params.id + ' found');
-});
+}
 
-app.patch('/translation/accept/:id', function(req, res) 
+app.put('/v2.0/translation/accept/:id', accept);
+app.patch('/v2.0/translation/accept/:id', accept);
+
+/*********************************************************************************************************/
+/* 
+ * Create a new request 
+ * can be used for post and put with
+ */
+ 
+function createNewRequest(req, res) 
 {
-	var i = findTranslationRequestIndex(req.params.id); // need to adapt to the real id!
-	if (i != null)
-	{
-		translationRequests[i].translationRequest.status =					"accept";
-		translationRequests[i].translationRequest.modificationDate =		new Date();
-		
-		if (translationRequests[i].translationRequest.updateCounter == undefined)
-			translationRequests[i].translationRequest.updateCounter = 1;
-		else if (translationRequests[i].translationRequest.updateCounter == null)
-			translationRequests[i].translationRequest.updateCounter = 1;
-		else
-			translationRequests[i].translationRequest.updateCounter =		translationRequests[i].translationRequest.updateCounter + 1;
-		saveTranslationRequests();
-		res.statusCode = 200;
-		res.json(translationRequests[i]);
-		return;
-	}
 
-	res.statusCode = 404;
-	return res.send('Error 404: No translationRequest ' + req.params.id + ' found');
-});
-
-app.post('/translation/', function(req, res) 
-{
-	/*
-	if(!req.body.hasOwnProperty('translationRequest')) 
-		// || !req.body.hasOwnProperty('targetLanguage') 
-		// || !req.body.hasOwnProperty('source')) 
-	{
-		res.statusCode = 400;
-		return res.send('Error 400: Post syntax incorrect.');
-	}
-	
-	*/
-	
 	var request = req.body.translationRequest;
 	if (request == undefined)
 	{
 		console.log(req);
 		res.statusCode = 400;
-		return res.send('Error 400: Post syntax incorrect. translationRequest for POST property missing');
+		return res.send('Error 400: Post/Put syntax incorrect. translationRequest for POST property missing');
 	}
 	console.log(req);
 	// console.log(req.body);
@@ -675,9 +547,18 @@ app.post('/translation/', function(req, res)
 	saveTranslationRequests();
 	res.statusCode = 201;
 	res.json(newQuote);
-});
+}
 
-app.delete('/translation/:id', function(req, res) 
+app.post('/v2.0/translation/', createNewRequest);
+app.put('/v2.0/translation/', createNewRequest); // just for security, not really needed
+
+/*********************************************************************************************************/
+/* 
+ * Delete a  request 
+ * requires an id
+ */
+
+app.delete('/v2.0/translation/:id', function(req, res) 
 {
 	var index = findTranslationRequestIndex(req.params.id) 
 	if (index != null)
@@ -694,5 +575,7 @@ app.delete('/translation/:id', function(req, res)
 	return res.send('Error 404: No translationRequest ' + req.params.id + ' found');
 
 });
+
+/*********************************************************************************************************/
 
 app.listen(process.env.PORT || 3412);
