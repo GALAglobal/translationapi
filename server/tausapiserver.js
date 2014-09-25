c5cb3be37a250528eca6ed73ebe9a13d9cce3bdf
Dr. Klemens Waldhör <info@heartsome.de>
2014-09-25 17:26:51 +0200

    v2.0c
    Check request attributes and values

--- server/tausapiserver.js
+++ server/tausapiserver.js
@@ -26,6 +26,7 @@ SOFTWARE.
 /*
 Author: Klemens Waldhör
 Version 2.0b / 24.09.2014
+Version 2.0c / 25.09.2014
 
 TAUS Translation API – Version 2.0
 TAUS Technical Specification -­ A Common Translation Services API - August 2014
@@ -48,6 +49,8 @@ TAUS Technical Specification -­ A Common Translation Services API - August 2014
  * - Path now starts with 2.0
  * - moved various functions outside the direct call app.put('/v2.0/translation/', function(req, res)) to app.put('/v2.0/translation/confirm', confirm) with confirm a function object 
  * - added comments
+ * v2.0c
+ * - attribute check for post, put of request
 */
 
 
@@ -137,6 +140,132 @@ else
 
 	];
 }
+
+/*********************************************************************************************************/
+
+var allowedAttributes =
+{
+	translationRequest:
+		{
+			id: "id",
+			callbackURL: "url",
+			links: "links",
+			sourceLanguage: "language",
+			targetLanguage: "language",
+			source: "string",
+			target: "string",
+			mt: ["true", "false"],
+			crowd: ["true", "false"],
+			professional: ["true", "false"],
+			postedit: ["true", "false"],
+			comment: "string",
+			translator: "string",
+			owner: "string",
+			creationDatetime: "date",
+			modificationDatetime: "date",
+			updateCounter: "int",
+			status: ["initial", "translated", "reviewed", "final", "rejected", "accepted", "pending", "timeout"]
+		},
+	comment:
+		{
+			id: "id",
+			referenceId: "id",
+			links: "links",
+			callbackURL: "url",
+			language: "language",
+			text: "string",
+			creationDatetime: "date"
+		},
+	score:
+		{
+			id: "id",
+			requestId: "id",
+			links: "links",
+			callbackURL: "url",
+			score: "int",
+			mt: ["true", "false"],
+			crowd: ["true", "false"],
+			professional: ["true", "false"],
+			text: "string",
+			creationDatetime: "date"
+		},
+	callbackRequest:
+		{
+			id: "id",
+			requestId: "id",
+			links: "links",
+			callbackURL: "url",
+			score: "int",
+			mt: ["true", "false"],
+			crowd: ["true", "false"],
+			professional: ["true", "false"],
+			callbackText: "string",
+			callbackStatus: "string",
+			callBackcreationDatetime: "date"
+		}
+}
+
+function checkAttribute(method, attribute, value)
+{
+	var reqallowed = allowedAttributes[method];
+	value = value + ""; // make a string anyway for the value
+	// console.log("checkAttribute:\n" + reqallowed);
+	for (var allowedprop in reqallowed)
+	{
+		// console.log(allowedprop);
+		if (!reqallowed.hasOwnProperty(allowedprop))
+		{
+			//The current property is not a direct property of p
+			continue;
+		}
+		if (allowedprop == attribute)
+		{
+			// check value - very explicit here
+			var allowedvalue= reqallowed[allowedprop];
+			if (allowedvalue instanceof Array)
+			{
+				for (var i = 0; i < allowedvalue.length; i++) 
+				{
+					if (allowedvalue[i] == value)
+						return true;
+				}
+				return false;
+			}
+			else
+				return true;
+		}
+	}
+	return false;
+}
+
+function checkAttributes(method, request)
+{
+	if (request == undefined)
+	{
+		return [false, 400, "Undefined request method", 1000];
+	}
+	// console.log("checkAttributes:\n" + method + "\nRequest:" + request);
+	for (var prop in request)
+	{
+		if (!request.hasOwnProperty(prop))
+		{
+			//The current property is not a direct property of p
+			continue;
+		}
+		var value = request[prop];
+		var attok = checkAttribute(method, prop, value);
+		if (attok == false)
+		{
+			return [false, 400, "\"" + prop + "\" with value \"" + value + "\" not allowed for request \"" + method + "\"", 1010];
+		}
+	}
+	return [true, 200, "", 0]; // correct request
+}
+
+
+
+/*********************************************************************************************************/
+
 
 // save translation request to file
 
@@ -267,11 +396,24 @@ app.get('/v2.0/translation/status/:id', function(req, res)
 function update(req, res) 
 {
 	var request = req.body.translationRequest;
+	var method = req.method;
+	var url = req.url;
 	if (request == undefined)
 	{
 		console.log(req);
 		res.statusCode = 400;
 		return res.send('Error 400: Put/Patch syntax incorrect. translationRequest for PUT property missing');
+	}
+	
+	var correctAttributes = checkAttributes("translationRequest", request);
+	var d = new Date();
+	var id = generateUUID();
+	console.log(correctAttributes);
+	// return [false, 400, "\"" + prop + "\" with value \"" + value + "\" not allowed for request \"" + method + "\""];
+	if (correctAttributes[0] == false)
+	{
+		res.statusCode = correctAttributes[1];
+		res.json( { error: { id: id, requestId: req.params.id, dateTime: d, errorMessage: correctAttributes[2], httpCode: correctAttributes[1], errorCode: correctAttributes[3], method: method, url: url, request: "translationRequest" } });
 	}
 	
 	var i = findTranslationRequestIndex(req.params.id); // need to adapt to the real id!
@@ -462,8 +604,9 @@ app.patch('/v2.0/translation/accept/:id', accept);
  
 function createNewRequest(req, res) 
 {
-
 	var request = req.body.translationRequest;
+	var method = req.method;
+	var url = req.url;
 	if (request == undefined)
 	{
 		console.log(req);
@@ -474,9 +617,18 @@ function createNewRequest(req, res)
 	// console.log(req.body);
     // console.log(req.files);
 
+	var correctAttributes = checkAttributes("translationRequest", request);
 	var d = new Date();
-	
 	var id = generateUUID();
+	console.log(correctAttributes);
+	// return [false, 400, "\"" + prop + "\" with value \"" + value + "\" not allowed for request \"" + method + "\""];
+	if (correctAttributes[0] == false)
+	{
+		res.statusCode = correctAttributes[1];
+		res.json( { error: { id: id, dateTime: d, errorMessage: correctAttributes[2], httpCode: correctAttributes[1], errorCode: correctAttributes[3], method: method, url: url, request: "translationRequest" } });
+	}
+	
+
 	var url = "http://" + req.headers.host; // how to get the http part of host???? e.g. if it is https??
 	// this are some test links will require a description how to define them depending on request method, status ...
 	var links = 
@@ -579,3 +731,4 @@ app.delete('/v2.0/translation/:id', function(req, res)
 /*********************************************************************************************************/
 
 app.listen(process.env.PORT || 3412);
+console.log("Listening on port " + 3412);
